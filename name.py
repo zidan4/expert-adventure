ed262a10134721700342be94f78da5022a24063e
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2026-02-13 21:33:02 +0300

    Create name.py

---
+++ name.py
@@ -0,0 +1,8 @@
+  def request_handler(buffer):
+  # perform packet modifications
+  return buffer
+  # modify any responses destined for the local host
+
+def response_handler(buffer):
+  # perform packet modifications
+  return buffer
